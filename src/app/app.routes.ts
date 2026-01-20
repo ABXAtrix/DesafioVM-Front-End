2ffb618d68548b4153b7fd58f9d9309dff3d705e
GuilhermeMacedoLL <macedo.lac157@gmail.com>
2026-01-20 17:41:03 -0300

    Implementação da tela de login e funcionalidade de registrar no sistema concluidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  
 
-//TODO: Adicionar rotas de outros módulos
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
+];
